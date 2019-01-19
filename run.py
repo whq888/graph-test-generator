9c0600018e6166bb4a19643006e7148a01f34244
zyt <ijumper@163.com>
2019-01-19 21:30:30 +0800

    init version

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: zengyuetian
+
+from libs.generator import *
+
+
+if __name__ == '__main__':
+    generator = Generator("ivis")
+    generator.generate()
